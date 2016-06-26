6f9c3a1b9cd4e1bb323f432a94ca3188c35f45b7
Jay Dorsey <me@github.jaydorsey.com>
2016-06-26 12:39:35 -0400

    Checkpoint: Saving google sign in code (non functioning)
    Setting this aside for now because we're switching away from Google
    sign in, but not completely abandoning it because we might
    integrate it again

---
+++ app/assets/javascripts/google_sign_in.js
@@ -0,0 +1,6 @@
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+}
